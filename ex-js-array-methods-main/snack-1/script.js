8c8db61a52e34e90a15ea285f4924d020990b718
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-04-22 15:56:52 +0200

    add-snack1

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -1,4 +1,8 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
+const names = ["Edoardo", "Simone", "Francesco"];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+const group = names.map((name) => {
+  return name;
+});
+console.log(group);
